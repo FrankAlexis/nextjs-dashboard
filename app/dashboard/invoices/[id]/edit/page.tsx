72c866bd6777dbf38062dcf12941fa9012e9c548
Frank Alexis Castrillom Giraldo <franka@Franks-MacBook-Pro.local>
2024-06-11 12:10:51 -0500

    (feat): Add final project files

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices Edit',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -11,7 +16,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     ]);
 
     console.log('invoice', invoice)
-    
+
     if (!invoice) {
         notFound();
     }
@@ -20,12 +25,12 @@ export default async function Page({ params }: { params: { id: string } }) {
         <main>
             <Breadcrumbs
                 breadcrumbs={[
-                { label: 'Invoices', href: '/dashboard/invoices' },
-                {
-                    label: 'Edit Invoice',
-                    href: `/dashboard/invoices/${id}/edit`,
-                    active: true,
-                },
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
                 ]}
             />
             <Form invoice={invoice} customers={customers} />
